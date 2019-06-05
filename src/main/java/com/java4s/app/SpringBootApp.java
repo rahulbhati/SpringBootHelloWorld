d248c13c74d58ad6f12b104beff14bf433336330
Rahul Bhati <rahulbhati0042@gmail.com>
2019-06-05 12:30:02 +0530

    Packaging with war
    Packaging with war

--- src/main/java/com/java4s/app/SpringBootApp.java
+++ src/main/java/com/java4s/app/SpringBootApp.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootApp {
+public class SpringBootApp extends SpringBootServletInitializer {
 	public static void main(String[] args) 
         {
 		 SpringApplication.run(SpringBootApp.class, args);
